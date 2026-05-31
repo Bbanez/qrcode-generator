989a67c74a35b7770b08baff5d0d60fca0de1140
bbanez <banezprod@gmail.com>
2026-05-31 22:43:12 +0200

    add nan check

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -45,25 +45,41 @@ export class Color {
     }
 
     static fromHex(hex: string): Color | null {
-        if (hex.startsWith('#')) {
-            hex = hex.slice(1);
-        }
-        if (hex.length === 3) {
-            const r = parseInt(hex[0] + hex[0], 16);
-            const g = parseInt(hex[1] + hex[1], 16);
-            const b = parseInt(hex[2] + hex[2], 16);
-            return new Color(r, g, b);
-        } else if (hex.length === 6) {
-            const r = parseInt(hex.slice(0, 2), 16);
-            const g = parseInt(hex.slice(2, 4), 16);
-            const b = parseInt(hex.slice(4, 6), 16);
-            return new Color(r, g, b);
-        } else if (hex.length === 8) {
-            const r = parseInt(hex.slice(0, 2), 16);
-            const g = parseInt(hex.slice(2, 4), 16);
-            const b = parseInt(hex.slice(4, 6), 16);
-            const a = parseInt(hex.slice(6, 8), 16);
+        try {
+            if (hex.startsWith('#')) {
+                hex = hex.slice(1);
+            }
+            let r = 0,
+                g = 0,
+                b = 0,
+                a = 255;
+            if (hex.length === 3) {
+                r = parseInt(hex[0] + hex[0], 16);
+                g = parseInt(hex[1] + hex[1], 16);
+                b = parseInt(hex[2] + hex[2], 16);
+            } else if (hex.length === 6) {
+                r = parseInt(hex.slice(0, 2), 16);
+                g = parseInt(hex.slice(2, 4), 16);
+                b = parseInt(hex.slice(4, 6), 16);
+            } else if (hex.length === 8) {
+                r = parseInt(hex.slice(0, 2), 16);
+                g = parseInt(hex.slice(2, 4), 16);
+                b = parseInt(hex.slice(4, 6), 16);
+                a = parseInt(hex.slice(6, 8), 16);
+                return new Color(r, g, b, a);
+            }
+            if (isNaN(r)) {
+                r = 0;
+            }
+            if (isNaN(g)) {
+                g = 0;
+            }
+            if (isNaN(b)) {
+                b = 0;
+            }
             return new Color(r, g, b, a);
+        } catch (err) {
+            console.error(`Invalid hex color: ${hex}\n`, err);
         }
         return null;
     }
